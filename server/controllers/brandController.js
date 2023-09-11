39d8367e39e716fe4aa08471ad6eba147df98ef9
kmedvedev <vasilsik951@gmail.com>
2023-09-11 16:52:20 +0300

    brand & type controller

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -1,9 +1,15 @@
+const { Brand } = require('../models/models');
+const { ApiError } = require('../error/apiError');
+
 class BrandController {
   async create(req, res) {
-
+    const { name } = req.body;
+    const brands = await Brand.create({ name });
+    return res.json(brands);
   }
   async getAll(req, res) {
-
+    const brands = await Brand.findAll();
+    return res.json(brands);
   }
 }
 
